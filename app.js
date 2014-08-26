d9ca57001d35c8fd3cde338b0a44ec0bb023236f
Kjell Ivar Storstein <kjell.ivar.storstein@visma.com>
2014-08-26 20:03:13 +0200

    new key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('InfoScreen', ['ngResource', 'googleApi'])
     .config(function(googleLoginProvider) {
         googleLoginProvider.configure({
-            clientId: '239511214798.apps.googleusercontent.com',
+            clientId: '301251250276-igdiqg8a5f7v1ph352lis9gu0mo5k4kj.apps.googleusercontent.com',
             scopes: ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/plus.login"]
         });
     });
